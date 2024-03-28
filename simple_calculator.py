4702d9c793cb98b597e07efa898aa9092029f606
AdinLamport <108032137+Alamport@users.noreply.github.com>
2024-03-28 00:01:00 -0400

    Update simple_calculator.py

--- simple_calculator.py
+++ simple_calculator.py
@@ -6,8 +6,34 @@ Description: A simple calculator able to do various mathematical operations
 Topics:
         - Functions
         - Conditionals
-        - Loops
         - Basic Recursion
 """
+def addition(num1, num2):
+        return num1 + num2
 
-def main()
+def subtraction(num1, num2):
+        return num1 - num2
+
+def multiplication(num1, num2):
+        return num1 * num2
+
+def division(num1, num2):
+        return num1 / num2
+
+def main():
+        num1 = int(input("Enter a number: "))
+        num2 = int(input("Enter a second number: "))
+        operation = input("Enter an operation (+|-|*|/) ")
+
+        if operation == '+':
+                print(addition(num1, num2))
+        elif operation == '-':
+                print(subtraction(num1, num2))
+        elif operation == '*':
+                print(multiplication(num1, num2))
+        elif operation == '/':
+                print(division(num1, num2))
+        else:
+                print("Try again and enter an operation that is correct")
+
+main()
